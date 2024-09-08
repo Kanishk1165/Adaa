410aee9031b3a6413e3bde1f860e8dfe1b9af7ac
Kanishk1165 <142769929+Kanishk1165@users.noreply.github.com>
2024-09-08 11:54:14 +0530

    Add files via upload
    Calculator using html css and java script

---
+++ js.js
@@ -0,0 +1,21 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteChar() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function append(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
